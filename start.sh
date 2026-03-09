3a06fa9d07e22a53681866b9086c8636e163fb55
mathyptr <mathyptr@gmail.com>
2026-03-09 16:12:45 +0100

    Update script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export script_dir="./script"
 
 #######If you have a GPU you have to execute the following script
-$script_dir/stopKAG-OllamaGPU.sh
+$script_dir/startKAG-OllamaGPU.sh
 #######otherwise if you have not a GPU you you have to execute the following script
-#$script_dir/stopKAG-OllamaGPU.sh
+#$script_dir/startKAG-OllamaGPU.sh
