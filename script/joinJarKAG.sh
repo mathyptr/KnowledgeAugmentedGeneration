41ff98c0630339f0684567c9a1f451a3ffbbefdd
mathyptr <mathyptr@gmail.com>
2026-03-17 13:37:45 +0100

    Add openspg jar

---
+++ script/joinJarKAG.sh
@@ -0,0 +1,1 @@
+cat server-jar/arks-sofaboot-0.0.1-SNAPSHOT-executable.jar_* > build/target/arks-sofaboot-0.0.1-SNAPSHOT-executable.jar
---
+++ script/splitJarKAG.sh
@@ -0,0 +1,1 @@
+split -b 50M arks-sofaboot-0.0.1-SNAPSHOT-executable.jar server-jar/arks-sofaboot-0.0.1-SNAPSHOT-executable.jar_
